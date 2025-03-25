df280a60c8811e9a4dd22869c2b25dbddd6474de
Yigit <mykayam794@gmail.com>
2025-03-25 23:31:29 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
+    <>
+    <StatusBar style="light" />
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Kurs Ekle" color="red" />
     </View>
+    </>
   );
 }
 
